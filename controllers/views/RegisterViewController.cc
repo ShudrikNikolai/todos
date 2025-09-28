5175b5ac7c78c3fa38ab023c2bbdecf50bc7a7f0
ShudrikNikolai <anonnik5@gmail.com>
2025-09-28 17:02:09 +0300

    feat: add views and [upd] configs

---
+++ controllers/views/RegisterViewController.cc
@@ -0,0 +1,6 @@
+#include "RegisterViewController.h"
+
+void RegisterViewController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
---
+++ controllers/views/TodoViewController.cc
@@ -0,0 +1,6 @@
+#include "TodoViewController.h"
+
+void TodoViewController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
